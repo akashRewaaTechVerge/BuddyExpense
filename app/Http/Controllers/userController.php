e35f9c3818c27313e29e40277297b31caa3a1255
unknown <ajay.rewaatechverge@gmail.com>
2022-02-08 17:19:34 +0530

    initial commint

---
+++ app/Http/Controllers/userController.php
@@ -0,0 +1,19 @@
+<?php
+ 
+namespace App\Http\Controllers;
+ 
+use Illuminate\Hashing\BcryptHasher;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
+use App\User;
+ 
+ 
+class UserController extends Controller
+{
+    public function login(Request $request)
+    { 
+        return  "string";
+    }
+ 
+  
+}
